3ad6ac85839ab68089515c24885232553550a340
Elmer <elmer.go@outlook.com>
2021-09-27 20:00:15 -0400

    Adding demos

--- bin/script.sh
+++ bin/script.sh
@@ -3,13 +3,17 @@
 CORE_COUNT=$1
 FILE=$2
 
-./bin/rarcrack --type zip --threads $CORE_COUNT $FILE
-hola () {
-    while true
-    do
-        echo hola;
-        sleep 1;
-    done
+main() {
+    ./bin/rarcrack --type zip --threads $CORE_COUNT $FILE
 }
 
-# hola
+demo() {
+    rm ./demo/demo.zip.xml
+    ./bin/rarcrack --type zip --threads $CORE_COUNT ./demo/demo.zip
+}
+
+if [ '$3' == '-d' ]; then
+    demo
+else
+    main
+fi
