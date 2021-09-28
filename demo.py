f3cba61cbac55c9ddc92d9718a6f7013b2e42c78
Elmer <elmer.go@outlook.com>
2021-09-27 21:17:23 -0400

    Actualizacion del modulo

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 import multiprocessing as mp
+from xml.dom import minidom
 import os
 
 router = APIRouter()
@@ -18,5 +19,23 @@ def run_demo():
         os.system(f'./bin/script.sh {cores} {file_name}')
 
     return {
-        'crack': 'running'
+        'demo': 'running'
+    }
+
+@router.get('/stop_demo')
+def stop_demo():
+    os.system(f'pkill rarcrack')
+
+    return {
+        'demo': 'stopped'
+    }
+
+@router.get('/current_pwd')
+def current_pwd():
+    file = minidom.parse('./file/VjEII3VODa2T.zip.xml')
+
+    current = file.getElementsByTagName('current')[0].firstChild.data
+
+    return {
+        'current_pwd': current
     }
